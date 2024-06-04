249d61607e183cb562ac4f99dcb52627345efe6d
SimonGideon <simongideon918@gmail.com>
2024-06-04 14:26:07 +0300

    fix: signin with username instead of email

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -18,13 +18,13 @@ class SessionsController < ApplicationController
       end
         
     def create
-        user = User.authenticate(params[:email], params[:password])
+        user = User.authenticate(params[:username], params[:password])
         if user
             puts "User found"
             session[:user_id] = user.id
             redirect_to home_path, notice: "Login Success"
         else
-            flash[:alert] = "Invalid email or password"
+            flash[:alert] = "Invalid username or password"
             render :new
         end
     end
@@ -38,6 +38,6 @@ class SessionsController < ApplicationController
     private
         
     def user_params
-        params.require(:user).permit(:email, :password)
+        params.require(:user).permit(:username, :password)
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,8 @@ class User < ApplicationRecord
     validates :password, presence: true, confirmation: true, on: :create
     validates :username, presence: true, uniqueness: true
 
-    def self.authenticate(email, password)
-        user = User.find_by(email: email)
+    def self.authenticate(username, password)
+        user = User.find_by(username: username)
         if user && user.password == password
             user
         else
